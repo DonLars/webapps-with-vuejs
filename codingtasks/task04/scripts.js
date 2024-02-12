a780011304ac5f931ea5a28b59a16af48617f2f7
Lars Wagner <lars@tubies.de>
2024-02-12 19:26:01 +0100

    feat: ads method and function with @click and :class

---
+++ codingtasks/task04/scripts.js
@@ -0,0 +1,35 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      activeLetter: "A",
+      opacityA: true,
+      opacityB: false,
+      opacityC: false,
+    };
+  },
+  methods: {
+    changeBtn(activeBtn) {
+      console.log(activeBtn);
+      this.activeLetter = activeBtn;
+
+      if (this.activeLetter === "A") {
+        this.opacityA = true;
+        this.opacityB = false;
+        this.opacityC = false;
+      }
+
+      if (this.activeLetter === "B") {
+        this.opacityA = false;
+        this.opacityB = true;
+        this.opacityC = false;
+      }
+      if (this.activeLetter === "C") {
+        this.opacityA = false;
+        this.opacityB = false;
+        this.opacityC = true;
+      }
+    },
+  },
+});
+
+app.mount("#app");
