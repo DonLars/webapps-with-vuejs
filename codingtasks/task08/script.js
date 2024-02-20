630392b2ff6f57d2730d99148ed822d558f541cb
Lars Wagner <lars@tubies.de>
2024-02-20 20:27:22 +0100

    feat: add deadSpot and fruit @click method

--- codingtasks/task08/script.js
+++ codingtasks/task08/script.js
@@ -14,22 +14,26 @@ Vue.createApp({
       ],
     };
   },
+  computed: {
+    mousePosition() {
+      return {
+        click: this.console,
+        mousemove: this.getPosition,
+      };
+    },
+  },
+  methods: {
+    console(event) {
+      //console.log(event);
+    },
+
+    getPosition(event) {
+      this.x = event.offsetX;
+      this.y = event.offsetY;
+    },
+
+    deleteFruit(event) {
+      event.target.parentElement.remove();
+    },
+  },
 }).mount("#app");
-
-/*
-In this task we use the Event Handling of vueJS.
-
-Add a mousemove event to the div container and update
- Update the data properties x and y with the current coordinates of the mouse position.
-offsetX and offsetY will be your friend
- Ensure that the coordinates getting displayed on the screen.
- There is a red dead spot in the middle of the area. The coordinates should not get updated when mouse moves over the spot.
-A modifier could help you to solve that issue.
-*/
-
-/*
-Given is a list of fruits but the remove button is not working yet.
- Make the remove button work.
- Take use of v-on directive and methods.
-
-*/
