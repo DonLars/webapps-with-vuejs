056a2e08ce089e8bd1eee48bc0ff4bcba3078a55
Lars Wagner <lars@tubies.de>
2024-02-23 12:54:06 +0100

    task completed

--- codingtasks/colormixervue/script.js
+++ codingtasks/colormixervue/script.js
@@ -1,28 +1,16 @@
 "use strict";
-const main = document.getElementById("main");
-const rgb = document.getElementById("rgb");
 
-const redInput = document.getElementById("red");
-const greenInput = document.getElementById("green");
-const blueInput = document.getElementById("blue");
-
-function updateBackgroundColor() {
-  const redValue = redInput.value;
-  const greenValue = greenInput.value;
-  const blueValue = blueInput.value;
-
-  const backgroundColor = `rgb(${redValue},${greenValue},${blueValue})`;
-  main.style.backgroundColor = backgroundColor;
-  rgb.textContent = backgroundColor;
-}
-redInput.addEventListener("input", updateBackgroundColor);
-greenInput.addEventListener("input", updateBackgroundColor);
-blueInput.addEventListener("input", updateBackgroundColor);
-
-main.style.backgroundColor = "hotpink";
-
-const picked = document.querySelector(".mixer-range");
-
-if (picked) {
-  updateBackgroundColor();
-}
+Vue.createApp({
+  data() {
+    return {
+      red: "150",
+      green: "180",
+      blue: "20",
+    };
+  },
+  computed: {
+    rgb() {
+      return `rgb(${this.red},${this.green},${this.blue})`;
+    },
+  },
+}).mount("#app");
